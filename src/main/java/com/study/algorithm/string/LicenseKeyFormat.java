384c5bdd73c4a10cd8dee65228c85f302bc4c622
Jade-Lee85 <weleek@epiens.com>
2021-09-24 18:37:25 +0900

    feat: 라이센스 키 포맷 구현중...

---
+++ src/main/java/com/study/algorithm/string/LicenseKeyFormat.java
@@ -0,0 +1,26 @@
+package com.study.algorithm.string;
+
+public class LicenseKeyFormat {
+
+    /**
+     * 숫자, 문자와 대시로맊 구성된 문자열로 표시된 라이센스 키가 제공 됩니다.
+     * 문자열은 대시(-)로 n 과 n + 1그룹으로 구분됩니다 . 정수값 k도 주어집니다.
+     * 첫 번째 그룹을 제외하고 각 그룹이 정확히 k로 문자열을 형식화(Formatting)해야 합니다.
+     * 첫 번째 그룹은 더 짧을 수 있지맊 여전히 적어도 하나의 문자를 포함해야합니다.
+     * 또한 두 그룹 사이에 대시가 삽입되어야하며 모든 소문자를 대문자로 변홖해야합니다.
+     * 재 포맷 된 라이센스 키를 리턴하세요
+     *
+     * 제한사항
+     * 라이선스 키 문자열은 1 이상 10의 5승 이하 입니다.
+     * 라이선스 키는 영문자, 숫자, 하이픈(-) 으로 이루어져 있습니다.
+     * 정수값 k는 1 이상 10의 4승 이하 입니다.
+     *
+     * 입출력 예
+     * licenseKey           formatLength            return
+     * 8F3Z-2e-9-w          4                       8F3Z-2E9W
+     */
+
+    public String solution(String licenseKey, int formatLength) {
+        return "";
+    }
+}
